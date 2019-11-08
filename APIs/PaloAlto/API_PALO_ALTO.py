673cca1341c2efa9541f181f92a0cab212c3b94b
reisvmr <reisvmr@gmail.com>
2019-11-08 15:43:43 -0300

    ok

--- APIs/PaloAlto/API_PALO_ALTO.py
+++ APIs/PaloAlto/API_PALO_ALTO.py
@@ -14,13 +14,14 @@ import urllib3
 
 chave = 'LUFRPT1idTV0YWlYbHd5ekozRjNOeE1kVnBHbC9lNUE9eTlGWXNncjJSekRpWE1MZ3hHcDVZYkhka3R5ZmoxSjZDV2RHTWxtOWtTUT0='
 url = 'https://10.32.208.101//api/?type=op&cmd=<show><system><info></info></system></show>&'
+categoriaurl = 'https://10.32.208.101/api/?type=report&async=yes&reporttype=predefined&reportname=top-url-categories&'
 #Payload contem todos os parametros que seram utilizados no post em forma de dicionario
 
 payload = { 'key' : chave}
 
 
 #print(response.get_text())
-r = requests.get(url, params=payload, verify=False)
+r = requests.get(categoriaurl, params=payload, verify=False)
 
 #################################################
 #############Inicio do Codigo####################
