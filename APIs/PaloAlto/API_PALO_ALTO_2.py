f057542f450b90904cd09ef3e3ee9d9b49ea64ab
reisvmr <reisvmr@gmail.com>
2020-02-12 11:00:58 -0300

    teste2

---
+++ APIs/PaloAlto/API_PALO_ALTO_2.py
@@ -0,0 +1,18 @@
+"""
+Created on Tue Aug 20 15:09:43 2019
+
+@author: viniciusreis
+pip install requests
+pip install simplejson
+pip install urllib3
+"""
+#######Importando Bibliotecas##################
+import requests
+#################################################
+
+############ Declarando as Variaveis ############
+KEY = 'LUFRPT1idTV0YWlYbHd5ekozRjNOeE1kVnBHbC9lNUE9eTlGWXNncjJSekRpWE1MZ3hHcDVZYkhka3R5ZmoxSjZDV2RHTWxtOWtTUT0='
+URL =
+IP_FW ='XX.XX.XX.XX'
+
+GET <firewall or Panorama IP>/restapi/9.0/<resource URI>?location=location&output-format=json
